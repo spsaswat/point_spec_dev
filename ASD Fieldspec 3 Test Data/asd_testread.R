c187b694addf6edb766268606c467c5cabfa1850
spsaswat <spsaswat16194@gmail.com>
2023-08-16 13:26:00 +1000

    bug fixed by Converting the spectra_for_comparison to a data.table

--- ASD Fieldspec 3 Test Data/asd_testread.R
+++ ASD Fieldspec 3 Test Data/asd_testread.R
@@ -10,17 +10,16 @@ spectral_data <- transpose(raw_ASD_text_export, make.names = "Wavelength")
 
 spectral_data <- cbind(Sample = 0, Group = 0, spectral_data)
 
-spectral_data$Sample <- colnames(spectra)[-1]
-
-# Calibration is the default first measurement (sample 000)
+spectral_data$Sample <- colnames(raw_ASD_text_export)[-1]
 spectral_data$Group <- c("calibration", "Solanaceae", "Solanaceae", "Myrtaceae", "Myrtaceae", "yellow leaf")
 
 spectra_for_comparison <- spectral_data[2:5, ] # get only our 4 actual leaves
 
-melted_spectra <- melt(spectra_for_comparison, id.vars = c("Group", 
-                                             "Sample"), variable.name = "wavelength", value.name = "reflectance")
+# Convert to data.table
+setDT(spectra_for_comparison)
 
-melted_spectra$wavelength=as.numeric(levels(melted_spectra$wavelength))[melted_spectra$wavelength]
+melted_spectra <- melt(spectra_for_comparison, id.vars = c("Group", "Sample"), variable.name = "wavelength", value.name = "reflectance")
+melted_spectra$wavelength = as.numeric(levels(melted_spectra$wavelength))[melted_spectra$wavelength]
 
 graph <- ggplot(data = melted_spectra, aes(x=wavelength, y=reflectance, group=Sample, color=Group)) +
   geom_line() +
